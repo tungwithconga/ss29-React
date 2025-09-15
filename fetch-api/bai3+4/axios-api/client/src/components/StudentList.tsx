181957f1fc1f21084ddf0d45d2b59838777f1d4f
tungwithconga <thanhtung100705@gmail.com>
2025-09-16 00:04:47 +0700

    first commit

--- fetch-api/bai3+4/axios-api/client/src/components/StudentList.tsx
+++ fetch-api/bai3+4/axios-api/client/src/components/StudentList.tsx
@@ -24,8 +24,42 @@ function StudentList() {
     }
   };
 
+  const getStudentById = async (id: number) => {
+    try {
+      const response = await axios.get<Student>(`http://localhost:3000/student/${id}`);
+      if (response.data) {
+        console.log("Chi tiết sinh viên:", response.data);
+      } else {
+        console.log("Không tìm thấy bản ghi");
+      }
+    } catch {
+      console.error("Không tìm thấy bản ghi");
+    }
+  };
+
+  const createStudent = async () => {
+    const newStudent: Omit<Student, "id"> = {
+      student_name: "Nguyen Van F",
+      email: "vanf@example.com",
+      address: "Quảng Ninh",
+      phone: "0906789123",
+      status: true,
+      created_at: new Date().toISOString()
+    };
+
+    try {
+      const response = await axios.post<Student>("http://localhost:3000/student", newStudent);
+      console.log("Sinh viên mới được thêm:", response.data);
+    } catch (error) {
+      console.error("Lỗi khi thêm sinh viên:", error);
+    }
+  };
+
   useEffect(() => {
     getAllStudent();
+    getStudentById(3);
+    getStudentById(99);
+    createStudent();
   }, []);
 
   return (
